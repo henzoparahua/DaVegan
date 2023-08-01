6191cd6e2314a0184ab3d941f43bcb84baab01ce
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-01 19:58:39 -0300

    making menu

--- app/src/main/java/br/com/etecia/davegan/Splash_activity.java
+++ app/src/main/java/br/com/etecia/davegan/Splash_activity.java
@@ -2,7 +2,9 @@ package br.com.etecia.davegan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class Splash_activity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Splash_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_layout);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(),
+                        Login_activity.class));
+                finish();
+            }
+        }, 3000);
     }
 }
