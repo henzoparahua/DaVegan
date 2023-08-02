4e2a2d8efa453457e4c39dffe2db939e13feb73c
henzoparahua <hnz.2894henzop19@gmail.com>
2023-08-01 21:43:57 -0300

    about to be done

--- app/src/main/java/br/com/etecia/davegan/Login_activity.java
+++ app/src/main/java/br/com/etecia/davegan/Login_activity.java
@@ -2,13 +2,38 @@ package br.com.etecia.davegan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
 
 public class Login_activity extends AppCompatActivity {
+
+    Button btnentrar;
+    TextInputEditText txtusu, txtpass;
+    TextView fyp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_layout);
+
+        btnentrar = findViewById(R.id.btnentrar);
+        txtusu = findViewById(R.id.txtusu);
+        txtpass = findViewById(R.id.txtpass);
+        fyp = findViewById(R.id.fyp);
+
+
+        btnentrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), Menu_activity.class));
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/com/etecia/davegan/Menu_activity.java
+++ app/src/main/java/br/com/etecia/davegan/Menu_activity.java
@@ -2,13 +2,27 @@ package br.com.etecia.davegan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Menu_activity extends AppCompatActivity {
+    TextView about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu_layout);
+
+        about = findViewById(R.id.about);
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),
+                        AboutUs_activity.class));
+            }
+        });
     }
 }
